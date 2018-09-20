a439e1123338174b41593aca512bfff1db6ccf51
jrichardson <jrichardson@freshbooks.com>
2018-09-20 11:24:09 -0400

    adding project to github

---
+++ DispatchGroup/TableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewController.swift
+//  DispatchGroup
+//
+//  Created by Justin Richardson on 2018-09-18.
+//  Copyright © 2018 Justin Richardson. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var value: UITableViewCell?
+        return value!
+    }
+
+}
+
